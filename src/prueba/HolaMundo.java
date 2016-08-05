08644e305c4e097a026a11a52a2240f220055207
tomasito <selefaccioli@gmail.com>
2016-08-05 19:51:27 -0300

    cambios menores

--- src/prueba/HolaMundo.java
+++ src/prueba/HolaMundo.java
@@ -3,6 +3,7 @@ package prueba;
 public class HolaMundo {
 
 	public static void main(String[] args) {
+		//bla
 		System.out.println("Hola Mundo");
 		System.out.println("SEGUNDA CLONACION HECHA POR TOMASITO");
 		System.out.println("HICE UN PULL!!! lml");
